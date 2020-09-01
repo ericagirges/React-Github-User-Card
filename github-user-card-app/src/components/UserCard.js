cbeec99380b1a356d516e71620f7e51e290b83a4
Erica Girges <ericafiorenza@gmail.com>
2020-09-01 15:48:06 -0700

    still need map to display followers and finish styling

--- github-user-card-app/src/components/UserCard.js
+++ github-user-card-app/src/components/UserCard.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import styled from "styled-components";
+
+const CardContainer = styled.div `
+    background-color: #363533;
+    color: white;
+    border-radius: 10px;
+    height: 500px;
+    width: 300px;
+    font-family: "Dosis";
+    font-weight: 500;
+    font-size: 1.5em;
+`
+
+const ProfileImage = styled.img `
+    height: 200px;
+    width: auto;
+    margin-top: 60px;
+`
 
 function UserCard(props) {
     return(
         <div>
-            <div>
-                <img src={props.user.avatar_url} alt="Erica Girges with brown hair ponytail"/>
+            <CardContainer>
+                <ProfileImage src={props.user.avatar_url} alt="Erica Girges with brown hair ponytail"/>
                 <h3>Name: {props.user.name}</h3>
                 <h4>Location: {props.user.location}</h4>
                 <h4>Followers: {props.user.followers}</h4>
                 <h4>Following: {props.user.following}</h4>
-            </div>
+            </CardContainer>
+            <CardContainer>
+                
+            </CardContainer>
 
         </div>
     )
