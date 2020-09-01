5bb0b375db0474fb79ede8584157887118bfe21a
Erica Girges <ericafiorenza@gmail.com>
2020-09-01 15:17:52 -0700

    USerCard progress

--- github-user-card-app/src/components/App.js
+++ github-user-card-app/src/components/App.js
@@ -1,16 +1,37 @@
 import React from "react";
 import "./App.css";
 import UserCard from "./UserCard";
+import axios from "axios";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      user: []
+      user: {},
+      followers: [],
     };
-    console.log('Constructor is running')
+    console.log("Constructor is running");
   }
 
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/ericagirges")
+      .then((response) => {
+        this.setState({
+          user: response.data,
+        });
+      })
+      .catch((error) => console.log(error));
+
+    axios
+      .get("https://api.github.com/users/ericagirges/followers")
+      .then((response) => {
+        this.setState({
+          followers: response.data,
+        });
+      })
+      .catch((error) => console.log(error));
+  }
 
   render() {
     return (
@@ -18,7 +39,7 @@ class App extends React.Component {
         <header className="App-header">
           <h1>GitHub Meet</h1>
         </header>
-        <UserCard />
+        <UserCard user={this.state.user} followers={this.state.followers}/>
       </div>
     );
   }
--- github-user-card-app/src/components/UserCard.js
+++ github-user-card-app/src/components/UserCard.js
@@ -3,6 +3,13 @@ import React from 'react';
 function UserCard(props) {
     return(
         <div>
+            <div>
+                <img src={props.user.avatar_url} alt="Erica Girges with brown hair ponytail"/>
+                <h3>Name: {props.user.name}</h3>
+                <h4>Location: {props.user.location}</h4>
+                <h4>Followers: {props.user.followers}</h4>
+                <h4>Following: {props.user.following}</h4>
+            </div>
 
         </div>
     )
